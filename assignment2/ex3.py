98e4a9f58644e71c69d75579587630ddc5e8d545
im-greysen <60055476+im-greysen@users.noreply.github.com>
2020-01-19 01:04:19 -0800

    Updated assignment2

---
+++ assignment2/ex3.py
@@ -0,0 +1,34 @@
+# this prints "I will now count my chickens:"
+print("I will now count my chickens:")
+
+# this calculates the number of hens
+print("hens", 25 + 30.0 / 6)
+# this calculates the number of Roosters
+print("Roosters", 100 - 25.0 * 3 % 4)
+# this calculates the number of eggs
+print("Now I will count the eggs:")
+
+# this calculates the value of the expression " 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6"
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# this asks a QUESTION:
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# this checks the validity of the argument of 5 < -2
+print(3 + 2 < 5 - 7)
+
+#this calculates 5
+print("What is 3 + 2?", 3.0 + 2)
+# this calculates -2
+print("What is 5 - 7?", 5.0 - 7)
+
+# this is supposed to be my internal monologue realizing why the argument above is False
+print("Oh, that's why it's False.")
+
+#more internal monologue
+print("How about some more.")
+
+# the last 3 lines of code checks the validity of the arguments
+print("Is it greater?", 5.0 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal", 5<= -2)
---
+++ assignment2/sd1.py
@@ -0,0 +1,5 @@
+print("Will the calulation 1 / 0 break a computer?")
+
+# I might end up saying the F-word.
+print("Here it goes", 1 / 0)
+# Whew! I was legitimately scared.
