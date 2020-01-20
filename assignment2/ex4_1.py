60d48a55932f63e01ceac0421de7f7c966228926
im-greysen <60055476+im-greysen@users.noreply.github.com>
2020-01-19 17:19:28 -0800

    ex4 through ex6 completed and started ex7

---
+++ assignment2/ex4_1.py
@@ -0,0 +1,7 @@
+x = 5
+y = x
+z = x - y
+k = x + y
+print("Time for another black hole.")
+
+k / z
---
+++ assignment2/ex6.py
@@ -0,0 +1,30 @@
+# establishes variable for crucial factor in setup
+types_of_people = 10
+# x sets up the joke
+x = f"There are {types_of_people} types of people."
+# binary is part of the punchline and creates a variable for the function
+binary = "binary"
+# similar to above this also is a variable in the puncline function
+do_not = "don't"
+# this defines the entire punchline as a function
+y = f"Those who know {binary} and those who {do_not}."
+# prints setup x
+print(x)
+# Prints puncline y
+print(y)
+# attempts to make bad joke funnier  by repeating setup
+print(f"I said: {x}")
+# above comment, but replace set up with punchline
+print(f"I also said: '{y}'")
+#creates variable for joke_evaluation
+hilarious = False
+# evaluates the bad joke
+joke_evaluation = "Isn't that joke so funny?! {}"
+# printsa the fact that the joke is bad and zed should feel bad for making me write it
+print(joke_evaluation.format(hilarious))
+# creates variable to  talk more about strings
+w = "this is the left side of..."
+# creates variable to complete string
+e = "a string with a right side."
+# prints string 
+print(w + e)
